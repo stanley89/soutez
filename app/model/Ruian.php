2c7e43d1ac23d836da6b99d16a99b691ec213911
Stanislav Stipl <stanislav.stipl@gmail.com>
2014-03-11 16:28:53 +0100

    kod

--- app/model/Ruian.php
+++ app/model/Ruian.php
@@ -40,7 +40,7 @@ class Ruian {
     }
     public function getCpByObecPairs($obec_kod) {
         $rows = $this->db->fetchAll("SELECT * FROM rn_adresni_misto a, rn_obec o
-                                    WHERE o.obec_kod=? AND ST_Intersects(a.definicni_bod,o.hranice) ORDER BY cislo_domovni;",$obec_kod);
+                                    WHERE o.kod=? AND ST_Intersects(a.definicni_bod,o.hranice) ORDER BY cislo_domovni;",$obec_kod);
         return $this->normalize($rows);
     }
     public function getCpByUlicePairs($ulice_kod) {
